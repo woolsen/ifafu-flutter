bd6d69c60623b2923d6d2fecccdfb2544428c3fa
wengkq <wengkq@hotmail.com>
2023-10-01 09:09:08 +0800

    新增长按复制联系方式效果

--- lib/widget/post.dart
+++ lib/widget/post.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ifafu/http/api.dart';
 import 'package:ifafu/http/model.dart' as model;
 import 'package:ifafu/page/post_detail_page.dart';
@@ -113,51 +114,53 @@ class _PostState extends State<Post> {
                     if (contactType != null &&
                         contactType.isNotEmpty &&
                         widget.post.contact != null)
-                      SelectableText(
-                        '${widget.post.contactType}: ${widget.post.contact}',
-                        autofocus: true,
-                        style: const TextStyle(fontSize: 16.0),
+                      GestureDetector(
+                        onTap: _copyContactToClipboard,
+                        onLongPress: _copyContactToClipboard,
+                        child: Text(
+                          '${widget.post.contactType}: ${widget.post.contact}',
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
                       ),
-                    if (widget.detailMode)
-                      Text(
+                    if (widget.detailMode && widget.post.content.isNotEmpty)
+                      SelectableText(
                         widget.post.content,
                         style: const TextStyle(
                           fontSize: 16.0,
                         ),
                       ),
-                    if (!widget.detailMode)
-                      if (widget.post.content.isNotEmpty)
-                        LayoutBuilder(
-                          builder: (context, constraints) {
-                            const maxLines = 5;
-                            const textStyle = TextStyle(fontSize: 16.0);
-                            var didExceed = didExceedMaxLines(
-                              widget.post.content,
-                              textStyle,
-                              maxLines,
-                              constraints.maxWidth,
-                            );
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  widget.post.content,
-                                  style: textStyle,
-                                  maxLines: maxLines,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                if (didExceed)
-                                  const Text(
-                                    '点击阅读更多',
-                                    style: TextStyle(
-                                      fontSize: 14.0,
-                                      color: Colors.blue,
-                                    ),
+                    if (!widget.detailMode && widget.post.content.isNotEmpty)
+                      LayoutBuilder(
+                        builder: (context, constraints) {
+                          const maxLines = 5;
+                          const textStyle = TextStyle(fontSize: 16.0);
+                          var didExceed = didExceedMaxLines(
+                            widget.post.content,
+                            textStyle,
+                            maxLines,
+                            constraints.maxWidth,
+                          );
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.post.content,
+                                style: textStyle,
+                                maxLines: maxLines,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              if (didExceed)
+                                const Text(
+                                  '点击阅读更多',
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: Colors.blue,
                                   ),
-                              ],
-                            );
-                          },
-                        ),
+                                ),
+                            ],
+                          );
+                        },
+                      ),
                     if (images != null && images.isNotEmpty) ...[
                       const SizedBox(height: 4.0),
                       ImageAlbum(
@@ -201,11 +204,13 @@ class _PostState extends State<Post> {
                           ),
                         const Spacer(),
                         if (!widget.detailMode)
-                          GestureDetector(
-                            onTap: _onCommentClick,
-                            child: const Icon(Icons.comment, size: 24.0),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            child: GestureDetector(
+                              onTap: _onCommentClick,
+                              child: const Icon(Icons.comment, size: 24.0),
+                            ),
                           ),
-                        const SizedBox(width: 8),
                         // GestureDetector(
                         //   onTap: () {
                         //     // Handle share click
@@ -218,6 +223,7 @@ class _PostState extends State<Post> {
                     if (!widget.detailMode &&
                         comments != null &&
                         comments.isNotEmpty) ...[
+                      const SizedBox(height: 4),
                       for (var comment in comments.take(4))
                         RichText(
                           text: TextSpan(
@@ -317,6 +323,15 @@ class _PostState extends State<Post> {
         );
       },
     );
+  }
+
+// 复制文本到剪贴板
+  void _copyContactToClipboard() {
+    if (widget.post.contact == null) {
+      return;
+    }
+    Clipboard.setData(ClipboardData(text: widget.post.contact!));
+    ToastUtil.show('联系方式已复制到剪贴板');
   }
 
   bool didExceedMaxLines(
